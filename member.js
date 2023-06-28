42b7e6cfee168be79d53c65893bda331c3f0ea37
Keitiankor <keitian.kor@gmail.com>
2023-06-28 21:44:50 +0900

    0

--- member.js
+++ member.js
@@ -14,6 +14,26 @@ $(function () {
         });
     });
 
+    $("#register").on("click", function () {
+        $.ajax({
+            url: "register",
+            type: "POST",
+            data: {
+                id: $("#idr").val(),
+                pw: $("#pwr").val(),
+                name: $("#name").val(),
+                tel: $("#tel").val(),
+            },
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (status, error) {
+                console.log(status);
+                console.log(error);
+            },
+        });
+    });
+
     $("#logout").on("click", function () {
         $.ajax({
             url: "logout",
