bd222950b0bdcd8f3d1ba9a98e50af1c39463790
Keitiankor <keitian.kor@gmail.com>
2023-06-29 18:36:03 +0900

    .

--- editpost.js
+++ editpost.js
@@ -1,12 +1,14 @@
 $(function () {
     $("#edit").on("click", function () {
+        let content = $("#content").var();
+
         $.ajax({
             url: "editpost",
             type: "POST",
             data: {
                 id: $("#id").text(),
                 title: $("#title").val(),
-                content: $("#content").val(),
+                content: content.replace(/(?:\r\n|\r|\n)/g, "<br>"),
             },
             success: function () {
                 location.href = "post?id=" + $("#id").text();
--- posting.js
+++ posting.js
@@ -1,11 +1,12 @@
 $(function () {
     $("#post").on("click", function () {
+        let content = $("#content").val();
         $.ajax({
             url: "posting",
             type: "POST",
             data: {
                 title: $("#title").val(),
-                content: $("#content").val(),
+                content: content.replace(/(?:\r\n|\r|\n)/g, "<br>"),
             },
             success: function () {
                 location.href = "bbs";
