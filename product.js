818f9c8921c2c1cc1cdf3cab31119873f5099140
Keitiankor <keitian.kor@gmail.com>
2023-06-29 13:20:24 +0900

    .

--- product.js
+++ product.js
@@ -1,17 +1,11 @@
 function before() {
     let page = new URL(location.href).searchParams.get("p");
     let beforepage = Number(page) - 1;
-    $.ajax({
-        url: "product?p=" + beforepage,
-        type: "GET",
-    });
+    location.href = "product?p=" + beforepage;
 }
 
 function next() {
     let page = new URL(location.href).searchParams.get("p");
     let nextpage = Number(page) + 1;
-    $.ajax({
-        url: "product?p=" + nextpage,
-        type: "GET",
-    });
+    location.href = "product?p=" + nextpage;
 }
