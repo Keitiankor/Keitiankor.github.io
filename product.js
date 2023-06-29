978b660ae8c070055d61f10b4a698e91151963b2
Keitiankor <keitian.kor@gmail.com>
2023-06-29 12:46:48 +0900

    .

--- product.js
+++ product.js
@@ -8,3 +8,14 @@ function before() {
         },
     });
 }
+
+function next() {
+    $.ajax({
+        url: product,
+        type: "GET",
+        data: {
+            limit: "10",
+            p: URLSearchParams.get("offset") - 1,
+        },
+    });
+}
