3b284545e05c11d1fa5bd08ce8b43fa7b65ea6d6
Keitiankor <keitian.kor@gmail.com>
2023-06-29 14:34:24 +0900

    .

--- post.js
+++ post.js
@@ -2,4 +2,21 @@ $(function () {
     $("#back").on("click", function () {
         location.href = "bbs";
     });
+
+    $("#edit").on("click", function () {
+        location.href = "editpost?id='#id'";
+    });
+
+    $("#delete").on("click", function () {
+        $.ajax({
+            url: "deletepost",
+            type: "POST",
+            data: {
+                id: sessionStorage.getItem("id"),
+            },
+            success: function () {
+                location.href = "bbs";
+            },
+        });
+    });
 });
